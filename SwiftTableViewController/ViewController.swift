86bd9d843a1b067024aa2140a3f5280e5a7550bb
Harry Bloom <harrybloom18@gmail.com>
2015-06-19 11:15:29 +0100

    Initial Commit

---
+++ SwiftTableViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTableViewController
+//
+//  Created by user29862 on 6/19/15.
+//  Copyright (c) 2015 Harry Bloom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
